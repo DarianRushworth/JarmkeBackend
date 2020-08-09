4b00c933f967900ba357ec630749e7d25c68852b
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-09 21:47:17 +0200

    Queries for user,order, product and category

---
+++ queries.js
@@ -0,0 +1,71 @@
+const User = require("./models").user
+// console.log("user test", User)
+const Order = require("./models").order
+// console.log("order test", Order)
+const Product = require("./models").product
+// console.log("product test", Product)
+const Category = require("./models").category
+// console.log("category test", Category)
+const OrderProduct = require("./models").orderProduct
+// console.log("orderProducts test", OrderProduct)
+const UserFavorite = require("./models").userFavorite
+// console.log("userFavorites test", UserFavorite)
+
+async function findUsers(){
+    try{
+        const response = await User.findAll()
+        // console.log("found user test", response)
+
+        const usersFound = response.map(user => 
+            user.get({
+                plain: true
+            })
+        )
+        console.log("plain user test", usersFound)
+
+    } catch(error){
+        console.log(error.message)
+    }
+}
+// findUsers()
+
+async function findProduct(productId){
+    try{
+        const response = await Product.findByPk(productId)
+        // console.log("product by ID", response)
+
+        const product = response
+                        ? response.get({
+                            plain: true
+                        })
+                        : "Product ID entered did not match please re-check and try again"
+        console.log("product test", product)
+
+
+    } catch(error){
+        console.log(error.message)
+    }
+}
+// findProduct(4)
+
+async function findOrders(){
+    try{
+        const response = await Order.findAll()
+        console.log("orders found test", response)
+
+    } catch(error){
+        console.log(error.message)
+    }
+}
+findOrders()
+
+async function findCategory(productId){
+    try{
+        const response = await Category.findByPk(productId)
+        console.log("category by title", response)
+
+    } catch(error){
+        console.log(error.message)
+    }
+}
+findCategory(4)
