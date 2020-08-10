277fe97665188c8b0ec696a50f0eba2de6ed9d08
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-10 20:40:38 +0200

    Favorites routes done, POST, DELETE, GET

--- routers/favoritesRouter.js
+++ routers/favoritesRouter.js
@@ -6,14 +6,41 @@ const UserFavorites = require("../models").userFavorite
 
 const router = new Router
 
+router.get(
+    "/favorites",
+    authMiddleWare,
+    async(req, res) => {
+        const userIdNeeded = parseInt(req.user.id)
+        // console.log("(GET)id test user:", userIdNeeded)
+
+        try{
+            const response = await UserFavorites.findAll({
+                where: {
+                    userId: userIdNeeded
+                }
+            })
+            // console.log("response test", response)
+
+            if(!response){
+                res.status(404).send("Oo gosh you've got no Favorites, better start clicking.")
+            }
+
+            res.status(202).send(response)
+
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+)
+
 router.post(
     "/products/:id",
     authMiddleWare,
     async(req, res) => {
         const userIdNeeded = req.user.id
-        console.log("id test user:", userIdNeeded)
+        // console.log("id test user:", userIdNeeded)
         const productIdNeeded = parseInt(req.params.id)
-        console.log("id test product:", productIdNeeded)
+        // console.log("id test product:", productIdNeeded)
         try{
             const response = await UserFavorites.create({
                 productId: productIdNeeded,
@@ -21,6 +48,37 @@ router.post(
             })
             // console.log("response test", response)
             res.status(202).send(response)
+
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+)
+
+router.delete(
+    "/products/:id",
+    authMiddleWare,
+    async(req, res) => {
+        const userIdNeeded = req.user.id
+        // console.log("(Delete)id test user:", userIdNeeded)
+        const productIdNeeded = parseInt(req.params.id)
+        // console.log("(Delete)id test product:", productIdNeeded)
+       
+        try{
+            const response = await UserFavorites.findOne({
+                where: {
+                    userId: userIdNeeded,
+                    productId: productIdNeeded
+                }
+            })
+            // console.log("Individual favorite test", response)
+
+            if(!response){
+                res.status(404).send("No Favorite matched your request.")
+            }
+
+            response.destroy()
+            res.status(202).send("Not your Favorite.")
 
         } catch(error){
             console.log(error.message)
