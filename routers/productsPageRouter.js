0ba8fb97f4e79cb12bb8a599098a0a05ecd66d03
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-10 17:57:26 +0200

    Route Setup for Specific Product

--- routers/productsPageRouter.js
+++ routers/productsPageRouter.js
@@ -5,6 +5,30 @@ const Products = require("../models").product
 const router = new Router()
 
 router.get(
+    "/products/:id",
+    async(req, res) => {
+        const idNeeded = parseInt(req.params.id)
+        try{
+            const response = await Products.findByPk(idNeeded)
+            // console.log("response test", response)
+
+            if(!response){
+                res.status(404).send("The product you are looking for has dissappeared, we are working on it")
+            }
+
+            const sendProduct = response
+                                ? response
+                                : "Loading..."
+            
+            res.status(202).send(sendProduct)
+
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+)
+
+router.get(
     "/products",
     async(req, res) => {
         try{
