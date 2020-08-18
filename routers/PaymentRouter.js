b9178bf099a84afd0103efae1591998b27eadff3
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-18 13:45:55 +0200

    Payment Sysytem in Place, Orders Revised

--- routers/PaymentRouter.js
+++ routers/PaymentRouter.js
@@ -1,25 +1,77 @@
 const { Router } = require("express")
 const stripe = require("stripe")("sk_test_51HGVaXE1l9Lb6wo5H7uIVCZzU63MKDYH9MAyPYyO5jwDgNm97bOicV4vOAcsOsuaW5vZPTnl6x4K6dDHILcqZySP00s7vIArU4")
 
+const Order = require("../models").order
+const User = require("../models").user
 const authMiddleWare = require("../auth/middleware")
 
 const router = new Router()
+
+router.patch(
+    "/payment",
+    authMiddleWare,
+    async(req, res) => {
+        const userIdNeeded = req.user.id
+
+        const { amount } = req.body
+        const intAmount = parseInt(amount)
+
+        const com = true
+        console.log("com test", com)
+        try{
+            const orderUpdate = await Order.update({
+                completed: com,
+            },{
+                where: {
+                    total: intAmount,
+                    userId: userIdNeeded,
+                    completed: false,
+                }
+            })
+            console.log("LETS SEE", orderUpdate)
+
+            const updatedUser = await User.findByPk(userIdNeeded,{
+                include: [Order],
+            })
+
+            res.status(202).send(updatedUser)
+
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+)
 
 router.post(
     "/payment",
     authMiddleWare,
     async(req, res) => {
+        const userIdNeeded = req.user.id
+
         const { amount, currency } = req.body
 
         const amountInt = parseInt(amount)
+
         try{
             const intent = await stripe.paymentIntents.create({
                 amount: amountInt,
                 currency,
+                metadata: {integration_check: 'accept_a_payment'},
             })
             console.log("what happened here", intent)
 
+            const newOrder = await Order.create({
+                total: 0,
+                userId: userIdNeeded,
+                productAmount: 0,
+                expressShipping: false,
+                shippingAddress: "User Address",
+                completed: false,
+            })
+            console.log("LETS NEW", newOrder)
+
             res.status(202).send({
+                order: newOrder,
                 client_secret: intent.client_secret
             })
 
