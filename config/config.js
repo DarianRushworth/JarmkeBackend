1df2022449648cba6261bc320bc6149e37298407
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-01 20:31:59 +0200

    operator aliases

--- config/config.js
+++ config/config.js
@@ -11,7 +11,8 @@ module.exports = {
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "postgres"
+    dialect: "postgres",
+    operatorsAliases: "0"
   },
   "production": { use_env_variable: "DATABASE_URL"}
 }
