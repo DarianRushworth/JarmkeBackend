9a0fd4a448cff81e15f37b7a24d8ff1bc946a7b3
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-01 19:33:35 +0200

    Deployment adjustments

--- config/config.js
+++ config/config.js
@@ -11,13 +11,7 @@ module.exports = {
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "pg"
+    "dialect": "postgres"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "pg"
-  }
+  "production": { use_env_variable: "DATABASE_URL"}
 }
