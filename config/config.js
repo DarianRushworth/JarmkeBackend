b1a53b886f6de7c480273d6b5e369b344c085cf2
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-01 20:23:17 +0200

    .env file dissappeared again

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
 require("dotenv").config()
 
 module.exports = {
-  "development": {
-    "url": process.env.DATABASE_URL,
-    "dialect": "postgres",
-    "operatorsAliases": "0"
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: "0"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "production": { use_env_variable: "DATABASE_URL"}
+  production: { use_env_variable: "DATABASE_URL"}
 }
